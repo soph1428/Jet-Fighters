0c93674f959de3eb86d1c37ae2202480b2f2708d
Sophia Groth <sophiagroth@textnow.me>
2021-09-20 17:37:34 -0500

    make it better

--- jetfightersserver.js
+++ jetfightersserver.js
@@ -4,7 +4,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/");
+  res.sendFile(__dirname + "/jetfighters.html");
 });
 
 app.use(express.static(__dirname));
